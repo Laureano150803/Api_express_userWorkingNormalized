b1aa684864505cf790e741a1d9325ab40bd14ebd
unknown <laureanohurtado1@gmail.com>
2023-07-31 18:58:53 -0500

    fixing 6

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -5,20 +5,27 @@ import User from "../models/User.js";
 passport.use(
     new passportJwt.Strategy({
         jwtFromRequest: passportJwt.ExtractJwt.fromAuthHeaderAsBearerToken(),
-        secretOrKey:process.env.SECRET
+        secretOrKey: process.env.SECRET
     },
-    async(jwt_payload, done)=>{
+    async (jwt_payload, done) => {
         try {
-            let user = await User.findOne({_id:jwt_payload.id})
-            if(user){
-                return done(null, user)
-            }else{
-                return done(null, false)
+            let user = await User.findOne({ _id: jwt_payload.id });
+            if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
             }
         } catch (error) {
-            console.log(error)
-            return done(error, false)
+            // Mejora: Pasa el error como primer argumento al método 'done'
+            return done(error, false);
         }
     })
-)
-export default passport
+);
+
+export default passport;
+
+
+
+
+
+
