04d2241b2d10c9b73f83ed3b0761694f151949a6
unknown <laureanohurtado1@gmail.com>
2023-07-31 18:42:55 -0500

    fixing 4

--- middlewares/accountSignIn.js
+++ middlewares/accountSignIn.js
@@ -3,24 +3,19 @@ import User from '../models/User.js'
 async function accountSignIn(req, res, next){
     const user = await User.findOne({email: req.body.email})
     if(user){
-        try {
-            req.user ={
-                id: user._id,
-                email:user.email,
-                photo:user.photo,
-                password: user.password,
-                role: user.role
-            }
-            return next()
-        } catch (error) {
-            return res.status(400).json({
-                status:400,
-                success:false,
-                Response:'user does not exist'
-            })
+        req.user ={
+            id: user._id,
+            email:user.email,
+            photo:user.photo,
+            password: user.password,
+            role: user.role
         }
-       
+        return next()
     }
-  
+    return res.status(400).json({
+        status:400,
+        success:false,
+        Response:'user does not exist'
+    })
 }
 export default accountSignIn
--- middlewares/passWordIsOk.js
+++ middlewares/passWordIsOk.js
@@ -4,16 +4,11 @@ function passwordIsOk(req, res, next){
     const db_pass = req.user.password
     const form_pass = req.body.password
     if(bcryptjs.compareSync(form_pass, db_pass)){
-        try {
-            return next()
-        } catch (error) {
-            return res.json({
-                status:400,
-                Response:'wrong password'
-            })
-        }
-        
+        return next()
     }
-   
+    return res.json({
+        status:400,
+        Response:'wrong password'
+    })
 }
 export default passwordIsOk
