c82dab13450521381667d22e34b719bcdea6c84f
unknown <laureanohurtado1@gmail.com>
2023-07-31 18:53:06 -0500

    fixing 6

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import 'dotenv/config.js'
 mongoose.set('strictQuery', false)
-mongoose.connect(process.env.CONECTION)
+mongoose.connect('mongodb+srv://antonio:antonio123@antonio.jret2ek.mongodb.net/')
 .then(()=>console.log('Conectado a mongo'))
 .catch(err => console.warn(err)) 
 
