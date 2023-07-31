d0ff0b80a9bd0ec961a37521d8e659cc1a8789c3
unknown <laureanohurtado1@gmail.com>
2023-07-31 18:31:02 -0500

    fixing 3

--- config/database.js
+++ config/database.js
@@ -1,14 +1,9 @@
-/* import mongoose from "mongoose";
-mongoose.set('strictQuery', false)
-mongoose.connect(process.env.CONECTION)
-.then(()=>console.log('Conectado a mongo'))
-.catch(err => console.warn(err)) */
-
 import mongoose from "mongoose";
+mongoose.set('strictQuery', false)
+const conexion = process.env.CONECTION
+console.log(typeof(conexion))
+mongoose.connect(conexion)
+.then(()=>console.log('Conectado a mongo'))
+.catch(err => console.warn(err)) 
 
-mongoose.connect(process.env.CONECTION, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('Conectado a MongoDB'))
-.catch(err => console.warn(err));
+
