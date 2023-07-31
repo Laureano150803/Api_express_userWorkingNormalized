870e7490d90d55b97eee195ede63275689549268
unknown <laureanohurtado1@gmail.com>
2023-07-31 18:19:37 -0500

    incluyendo trycatch

--- middlewares/accountSignIn.js
+++ middlewares/accountSignIn.js
@@ -3,19 +3,24 @@ import User from '../models/User.js'
 async function accountSignIn(req, res, next){
     const user = await User.findOne({email: req.body.email})
     if(user){
-        req.user ={
-            id: user._id,
-            email:user.email,
-            photo:user.photo,
-            password: user.password,
-            role: user.role
+        try {
+            req.user ={
+                id: user._id,
+                email:user.email,
+                photo:user.photo,
+                password: user.password,
+                role: user.role
+            }
+            return next()
+        } catch (error) {
+            return res.status(400).json({
+                status:400,
+                success:false,
+                Response:'user does not exist'
+            })
         }
-        return next()
+       
     }
-    return res.status(400).json({
-        status:400,
-        success:false,
-        Response:'user does not exist'
-    })
+  
 }
 export default accountSignIn
