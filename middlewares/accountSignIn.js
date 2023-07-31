2b02234c384f6fc218719426339764456d287bd9
unknown <laureanohurtado1@gmail.com>
2023-07-31 18:50:33 -0500

    fixing 5

--- middlewares/accountSignIn.js
+++ middlewares/accountSignIn.js
@@ -1,21 +1,31 @@
-import User from '../models/User.js'
+import User from '../models/User.js';
 
-async function accountSignIn(req, res, next){
-    const user = await User.findOne({email: req.body.email})
-    if(user){
-        req.user ={
-            id: user._id,
-            email:user.email,
-            photo:user.photo,
-            password: user.password,
-            role: user.role
+async function accountSignIn(req, res, next) {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if (user) {
+            req.user = {
+                id: user._id,
+                email: user.email,
+                photo: user.photo,
+                password: user.password,
+                role: user.role
+            };
+            return next();
         }
-        return next()
+        return res.status(400).json({
+            status: 400,
+            success: false,
+            Response: 'User does not exist'
+        });
+    } catch (error) {
+        // Manejar el error aquí
+        return res.status(500).json({
+            status: 500,
+            success: false,
+            Response: 'Internal Server Error'
+        });
     }
-    return res.status(400).json({
-        status:400,
-        success:false,
-        Response:'user does not exist'
-    })
 }
-export default accountSignIn
+
+export default accountSignIn;
