6e53aae62b209e3b544ce49eb0f9e505cb9d952e
unknown <laureanohurtado1@gmail.com>
2023-08-15 11:33:07 -0500

    xd

--- config/database.js
+++ config/database.js
@@ -1,8 +1,6 @@
 import mongoose from "mongoose";
 import 'dotenv/config.js'
 mongoose.set('strictQuery', false)
-const conexion = process.env.CONECTION
-console.log(typeof(conexion))
 mongoose.connect(process.env.CONECTION)
 .then(()=>console.log('Conectado a mongo'))
 .catch(err => console.warn(err)) 
