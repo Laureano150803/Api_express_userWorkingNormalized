70f8bfa9140ff4f82aa6030843a239310407f2fd
unknown <laureanohurtado1@gmail.com>
2023-08-18 11:49:34 -0500

    incorporando cors

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan'
 import './config/database.js';
-
+import cors from 'cors'
 import indexRouter from './routes/index.js'
 import { __dirname } from './utils.js';
 const app = express();
@@ -13,7 +13,7 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
