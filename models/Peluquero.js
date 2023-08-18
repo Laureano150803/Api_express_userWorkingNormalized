4f002279af37d869209dd866616289b7a2f912c2
Laureano Hurtado <109369745+Laureano150803@users.noreply.github.com>
2023-08-18 11:50:26 -0500

    incorporando cors (#2)

--- models/Peluquero.js
+++ models/Peluquero.js
@@ -1,8 +1,10 @@
-import mongoose from "mongoose";
+import mongoose, { Types } from "mongoose";
 
 const schema = new mongoose.Schema({
     nombre:{type:String, required:true},
-    especialidad:{type:String, required:true}
+    apellido:{type:String, required:true},
+    especialidad:{type:String, required:true},
+    user_id:{type:Types.ObjectId, ref:'users', required:true}
 
 },{
     timestamps: true
