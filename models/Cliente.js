d22a8c4ad9e29212641732ee4194963d8d4ee011
Laureano Hurtado <109369745+Laureano150803@users.noreply.github.com>
2023-08-18 23:46:15 -0500

    Lau (#4)
    * incorporando cors
    
    * terminado modelo ingePuri

--- models/Cliente.js
+++ models/Cliente.js
@@ -4,6 +4,7 @@ const schema = new mongoose.Schema({
     nombre:{type:String, required:true},
     apellido:{type:String, required:true},
     telefono:{type:String, required:true},
+    cedula:{type:String, required:true},
     user_id:{type:Types.ObjectId, ref:'users', required:true}
 },{
     timestamps: true
