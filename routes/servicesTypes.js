724dc8e0b6eb069544531d1ed081d11a128a59ff
Laureano Hurtado <109369745+Laureano150803@users.noreply.github.com>
2023-10-31 11:13:20 -0500

    Lau (#12)
    * incorporando cors
    
    * terminado modelo sisas
    
    * working
    
    * copia de seguridad
    
    * data inyectada
    
    * Citas funcionando
    
    * nuevo controlador para peluqueros
    
    * nuevo controlador

--- routes/servicesTypes.js
+++ routes/servicesTypes.js
@@ -1,8 +1,10 @@
 import express from "express";
 const router = express.Router()
 import read from "../controllers/servicesTypes/read.js";
+import getById from "../controllers/servicesTypes/getById.js";
 
 router.get('/', read)
+router.get('/:id', getById)
 
 
 export default router
