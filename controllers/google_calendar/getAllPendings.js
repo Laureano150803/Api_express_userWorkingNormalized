470d4b2dd33424dd214e391ff6fc430a12a5e6d8
yeison1526 <yesiontatiscampillo1526@gmail.com>
2023-11-13 22:09:32 -0500

    shema de Citas actualizado

--- controllers/google_calendar/getAllPendings.js
+++ controllers/google_calendar/getAllPendings.js
@@ -2,7 +2,7 @@ import Cita from "../../models/Cita.js";
 
 const allPendings = async(req, res)=>{
     try {
-        const all = await Cita.find({status:'PENDING'}).populate('cliente_id')
+        const all = await Cita.find({status:'PENDING'}).populate('cliente_id servicio_id ')
        
         if (all.length ===0) {
             return res.status(200).json({
@@ -17,6 +17,7 @@ const allPendings = async(req, res)=>{
             Response:all
         })
     } catch (error) {
+        console.log(error)
         return res.status(500).json({
             status:500,
             success:false,
