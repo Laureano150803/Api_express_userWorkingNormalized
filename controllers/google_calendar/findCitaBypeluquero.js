3b1f598aa52a92157999696c3324533226ad1c6d
unknown <ospinopupo@gmail.com>
2023-11-14 13:53:49 -0500

    updating some controllers

--- controllers/google_calendar/findCitaBypeluquero.js
+++ controllers/google_calendar/findCitaBypeluquero.js
@@ -1,15 +1,16 @@
 import Cita from '../../models/Cita.js'
-
+import Peluquero from '../../models/Peluquero.js'
 const citaByPeluquero = async (req, res, next) => {
     try {
-        const citas = await Cita.find({ peluquero_id: req.params.p_id })
-            .populate('peluquero_id cliente_id');
+        const peluquero = await Peluquero.findOne({user_id:req.user.id})
+        const citas = await Cita.find({ peluquero_id: peluquero._id})
+            .populate('servicio_id cliente_id');
          // Usar populate para cargar los detalles de peluquero y cliente
         if (citas) {
             return res.status(200).json({
                 status: 200,
                 success: true,
-                response:{citas:citas}
+                response:citas
             });
         } else {
             return res.status(404).json({
