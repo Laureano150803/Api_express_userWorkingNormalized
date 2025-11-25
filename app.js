5960cb3208f1a6ddde81e14bc5af1e657ee2d0e6
lauda <laureanohurtado1@gmail.com>
2025-11-25 13:02:22 -0500

    fix CORS

--- app.js
+++ app.js
@@ -19,7 +19,11 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(cors())
+app.use(cors({
+  origin: '*',
+  methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
