e8df3cc61da4d7d7ad76a4adbe8ce9288cd68a80
lauda <laureanohurtado1@gmail.com>
2025-11-25 12:13:18 -0500

    fix v1

--- routes/peluqueros.js
+++ routes/peluqueros.js
@@ -4,7 +4,7 @@ import create from "../controllers/peluqueros/create.js";
 import read from "../controllers/peluqueros/read.js";
 import deleteById from "../controllers/peluqueros/deleteById.js";
 import updateById from "../controllers/peluqueros/updateById.js";
-import getPeluquerosDisponibles from "../controllers/peluqueros/GetPeluquerosDisponibles.js";
+import getPeluquerosDisponibles from "../controllers/peluqueros/getPeluquerosDisponibles.js";
 import passport from "passport";
 import uploadImage from "../services/firebase.cjs";
 import Multer from "../middlewares/multer.js";
